d05236f4216619551e769aa0b455c8c89fd5ce9b
LDorantes <leodoor2@gmail.com>
2023-06-04 15:00:13 -0600

    Class Procet 1

---
+++ Projecto parcial 1/Projecto parcial 1/Array.cpp
@@ -0,0 +1,100 @@
+#include "Array.h"
+#include <iostream>
+
+
+//Constructor de la clase: el prametro "in_Size" se utiliza para determinar el tamano del array dinamico "InitialElement". 
+//"New init " funciona para asignar la memoria "Size" que utilizara el arreglo
+RawArray::RawArray(int in_Size) : Size(in_Size)
+{
+	InitialElement = new int[Size];
+
+	// just added 
+
+	InitialElement[0] = 1;
+	InitialElement[1] = 5;
+	InitialElement[2] = 9;
+	InitialElement[3] = 0;
+	InitialElement[4] = 1;
+	InitialElement[5] = 5;
+
+
+	// Restriccion del tamano del arreglo. 
+	if (Size > 6)
+	{
+		std::cout << "Advertencia: El tamaño especificado es mayor que el número de valores asignados. "\
+			"Los valores restantes se inicializarán a 0." << std::endl;
+
+
+		if (Size > 6)
+		{
+			std::cout << "Advertencia: El tamaño especificado es mayor que el nÃºmero de valores asignados. "\
+				"Los valores restantes se inicializarán a 0." << std::endl;
+
+
+
+			// Inicializar los valores restantes en 0
+			for (int i = 6; i < Size; i++)
+			{
+				InitialElement[i] = 0;
+			}
+		}
+	}
+}
+
+
+		//Destructor de la clase. Se encarga de liberar la memoria asignada  dinamicamente apra el arreglo.
+		RawArray::~RawArray()
+		{
+			std::cout << "Entrada destructor de RawArray" << '\n';
+			delete[] InitialElement;
+			std::cout << "Salida destructor" << '\n';
+		}
+
+			
+
+		
+	//Esta funcion se utiliza para inicializar todos los elementos del arreglo con un valor inicial. 
+	//InitialValue representa el valor con el que se desea inicializar los elementos. 
+	void RawArray::Initial(int InitialValue)
+	{
+
+		int* auxPosition = InitialElement;
+
+		//Utilizamos el for para rrecorrer los elementos del arreglo y asignarles el valor "InitialValue"
+		for (int i = 0; i < Size; i++)
+		{
+			*auxPosition = InitialValue;
+
+			auxPosition++;
+		}
+	}
+	//Esta funcion se utiliza para imprimir en la consola todos los valores de los elementos del arreglo.
+	void RawArray::Print()
+	{
+		int* auxPosition = InitialElement;
+
+		for (int i = 0; i < Size; i++)
+		{
+			std::cout << *auxPosition << ", ";
+
+			auxPosition++;
+		}
+		std::cout << '\n';
+	}
+
+	//Utiliza un puntero auxPosition para recorrer los elementos del arreglo y compara el valor del contenido de cada espacio de memoria con utilizando el operador *. 
+	//Si el valor X coincide, asigna Y al elemento mediante el operador de referencia *.
+	//Luego, incrementa el puntero auxPosition para avanzar al siguiente elemento y realizar la misma accion.
+
+	void RawArray::Replace(int X, int Y)
+	{
+		int* auxPosition = InitialElement;
+		for (int i = 0; i < Size; i++)
+		{
+			if (*auxPosition == X)
+			{
+				*auxPosition = Y;
+			}
+			auxPosition++;
+		}
+	}
---
+++ Projecto parcial 1/Projecto parcial 1/Projecto parcial 1.cpp
@@ -0,0 +1,43 @@
+// Projecto parcial 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Array.h"
+#include "Array.cpp"
+
+#define MY_NULL 0 
+
+int main()
+{
+    std::cout << "Hello Everyone\n";
+    // Para la realizacion no se incluyo una restriccion en el codigo si se intenta ingresar valores distintos de int debido a que no se considero necesario
+    // Pues esta restriccion se encuentra implicita en la declaracion del RawArray el cual unicamente recibe valores int.
+
+    RawArray myArray(9);
+    //myArray.Initial(5);
+
+
+    std::cout << "Original Array: \n";
+
+    myArray.Print();
+
+    myArray.Replace(5, 0);
+
+    std::cout << "Modified Array: \n";
+
+    myArray.Print();
+
+    return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
