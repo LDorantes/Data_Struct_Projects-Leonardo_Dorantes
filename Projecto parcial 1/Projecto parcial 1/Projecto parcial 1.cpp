5d1b43f416dd26fa08cb6122fd80b2f68e2755f5
Leoardo Dorantes <133131159+LDorantes@users.noreply.github.com>
2023-06-11 18:49:05 -0600

    Update Projecto parcial 1.cpp

--- Projecto parcial 1/Projecto parcial 1/Projecto parcial 1.cpp
+++ Projecto parcial 1/Projecto parcial 1/Projecto parcial 1.cpp
@@ -5,39 +5,112 @@
 #include "Array.h"
 #include "Array.cpp"
 
+
 #define MY_NULL 0 
 
 int main()
 {
+// Para la realizacion no se incluyo una restriccion en el codigo si se intenta ingresar valores distintos de int debido a que no se considero necesario
+// Pues esta restriccion se encuentra implicita en la declaracion del RawArray el cual unicamente recibe valores int.
     std::cout << "Hello Everyone\n";
-    // Para la realizacion no se incluyo una restriccion en el codigo si se intenta ingresar valores distintos de int debido a que no se considero necesario
-    // Pues esta restriccion se encuentra implicita en la declaracion del RawArray el cual unicamente recibe valores int.
 
-    RawArray myArray(9);
-    //myArray.Initial(5);
 
-    myArray.ArrayInside();
-    std::cout << "Original Array: \n";
+    RawArray myArray(10);
+
+    myArray.TestCaseReplace(1);
+ 
+    std::cout << " \nArray Original: \n";
+
+    myArray.Print(); 
+
+    myArray.Replace(9, 0);
+
+    std::cout << " \nArray Modificado: \n"; 
+
+    myArray.Print();
+    
+    //APARTADO PARA EL EXAMEN 
+
+    //AssignEveryXElements
+    std::cout << "\nApartado examen \n";
+    std::cout << "Funcion AssignEveryXElements \n";
+    std::cout << " \nArray Original: \n";
+    myArray.Print();
+    myArray.AssignEveryXElements(5, 2);
+    std::cout << " \nArray Modificado: \n";
+    myArray.Print();
+
+    //SortFunction
+    std::cout << "\nSortFunction\n";
+    std::cout << " \nArray Original: \n";
+    myArray.Print();
+    myArray.SortFunction();
+    std::cout << " \nArray Ordenado: \n";
+    myArray.Print();
+    
+    //GetIndexOf Function
+    std::cout << "\nGetIndexOf Function \n";
+    std::cout << "Array Original: \n";
+    myArray.Print(); 
+    int index = myArray.GetIndexOf(5);
+    std::cout << "La posicion del elemento elegido es: " << index << std::endl;
+    
+    //GetLastOf Function 
+    std::cout << "\nGetLastOf Function \n";
+    myArray.Print();
+    myArray.GetLastOf(5);
+    
+    //GetIndicesOf
+    std::cout << "\nGetIndicesOf Function \n";
+    myArray.GetIndicesOf(5);
+
+    //AppendArray Function 
+    std::cout << " \nArray Original: \n";
+    myArray.Print();
+    RawArray test = RawArray(3);
+    test.Initial(0);
+    std::cout << " \nArreglo a agregar \n";
+    test.Print();
+    myArray.AppendArray(test);
+    std::cout << " \nArray Final: \n";
+    myArray.Print();
+    
+    //Función Insert
+    std::cout << " \n Insert Function\n";
+    myArray.Initial(0);
+
+    RawArray toBeInserted = RawArray(3);
+    toBeInserted.Initial(1);
+
+    int currentStartIndex = 3;
+    myArray.Insert(toBeInserted, currentStartIndex);
 
     myArray.Print();
 
-    myArray.Replace(5, 0);
-
-    std::cout << "Modified Array: \n";
-
+    
+    //SetSize function
+    std::cout << " \n SetSize Function\n";
+    std::cout << " Original Array\n";
+    myArray.Print(); 
+    myArray.SetSize(5); 
+    std::cout << " Modified Arrays\n";
     myArray.Print();
+    myArray.SetSize(8);
+    myArray.Print();
+
+    //SumArrays Function
+    std::cout << "\nSumArrays Function\n";
+    std::cout << "Array A\n";
+    RawArray arrayA(3);
+    arrayA.Initial(3);
+    arrayA.Print(); 
+    std::cout << "Array B\n";
+    RawArray arrayB(3);
+    arrayB.Initial(2);
+    arrayB.Print();
+    arrayB.SumArrays(arrayA);
+    std::cout << "Resultado:\n";
+    arrayB.Print();
 
     return 0;
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
